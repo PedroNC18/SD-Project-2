73b1b9080ddaef04b68776e3dedf2c819b4c74e5
Zilde Neto <37539515+zsmn@users.noreply.github.com>
2018-11-29 00:07:49 -0200

    Update contador.v

--- contador.v
+++ contador.v
@@ -1,22 +1,32 @@
 module contador(clk, sel, tempo, segundos);
 	input clk; // negocio la
 	input sel; // selecao do que vai acontecer (0 : cronometro e 1 : temporizador)
-	input tempo; // quanto tempo queremos q rode
-	output segundos; // o numero que vai estar saindo do contador
+	input [15:0] tempo; // quanto tempo queremos q rode
+	output [15:0] segundos; // o numero que vai estar saindo do contador
 	reg[25:0] cont;
-	reg segundos;
+	reg [15:0] segundos;
 	
+	reg [25:0] piroquinha;
+	reg [15:0] segundim;
+	
+	reg flag;
 	initial begin
-		if(sel) segundos <= tempo;
-		else segundos <= 0; 
+		flag <= 0;
 	end
 	
 	always @ (posedge clk) begin
-		cont <= cont + 1;
-		if(cont == 26'b10111110101111000010000000) begin
+		if(!flag) begin
+			if(sel) segundos <= tempo;
+			else segundos <= 0;
+			flag <= 1;
+		end
+		piroquinha <= cont;
+		segundim <= segundos;
+		cont <= piroquinha + 1;
+		if(cont == 26'b01011111010111100001000000) begin
 			case(sel)
-				0: if(segundos < tempo) segundos <= segundos + 1;
-				1: if(segundos > 0) segundos <= segundos - 1;
+				0: if(segundos < tempo) segundos <= segundim + 1;
+				1: if(segundos > 0) segundos <= segundim - 1;
 			endcase
 			cont <= 0;
 		end
